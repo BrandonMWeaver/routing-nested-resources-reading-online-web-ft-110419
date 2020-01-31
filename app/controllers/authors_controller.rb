638f97bc45227eb38df54198307a946cb1ecc087
Brandon Weaver <a7xncob@gmail.com>
2020-01-31 20:53:10 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,13 +7,13 @@ class AuthorsController < ApplicationController
   def posts_index
     @author = Author.find(params[:id])
     @posts = @author.posts
-    render "posts/index"
+    render template: "posts/index"
   end
   
   def post
     @author = Author.find(params[:id])
     @post = Post.find(params[:post_id])
-    render "posts/show"
+    render template: "posts/show"
   end
   
 end
